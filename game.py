751868e65e573d777d182bce16ec89cc413c508f
jasmeenk04 <jasmeenkm04@gmail.com>
2025-05-28 19:06:28 -0700

    Win Message added

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ from tkinter import *
 import tkinter as tk
 import tkinter.font as font
 from fifteen import Fifteen
+from tkinter import messagebox 
+
 
 class Game:
     def __init__(self, master):
@@ -19,31 +21,37 @@ class Game:
         if value == "shuffle":
             self.board.shuffle()
             self.make_buttons()
-        elif self.board.is_valid_move(value):
+            return
+        try:
+            move = int(value)
+        except ValueError:
+            return
+            
+        if self.board.is_valid_move(value):
             self.board.update(value)
             self.make_buttons()
 
+            if self.board.is_solved():
+                messagebox.showinfo("You Win!", "Congratulations! You solved the puzzle!")
+
     def make_buttons(self):
         button_props = {'bg': 'white', 'fg': 'black', 'font': self.font, 'height': 2, 'width': 5}
+
         for i in range(16):
             text = tk.StringVar()
-            if self.board.tiles[i] == 0:
-                text.set('')
-            else:
-                text.set(str(self.board.tiles[i]))
-            name = str(self.board.tiles[i])
-            button = tk.Button(self.gui, textvariable=text, name=name, command=lambda value=name: self.click_button(value), **button_props)
-            if i == 0:
-                button.grid(row=0, column=0)
-            elif i % 4 == 0:
-                button.grid(row=i//4, column=0)
-            else:
-                button.grid(row=i//4, column=i%4)
-            if self.board.tiles[i] == 0:
+            tile_value = self.board.tiles[i]
+
+            display = '' if tile_value == 0 or tile_value == '  ' else str(tile_value)
+            text.set(display)
+
+            button = tk.Button(self.gui, textvariable=text, command=lambda v=tile_value: self.click_button(v), **button_props)
+
+            button.grid(row=i//4, column=i%4)
+
+            if tile_value == 0 or tile_value == '  ':
                 button.configure(bg='gray')
             else:
                 button.configure(bg='white')
-            self.gui.nametowidget(name).configure(bg='white')
 
         shuffle_button = tk.Button(self.gui, text="Shuffle", name="shuffle", command=lambda: self.click_button("shuffle"), **button_props)
         shuffle_button.grid(row=4, column=0)
